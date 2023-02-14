20850ba20c6d72d0889dce2141023ba0015f5022
Raulscj <92169825+Raulscj@users.noreply.github.com>
2023-02-13 23:58:53 -0400

    trabajando styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import Navbar from "./componentes/Nav";
 import PostForm from "./componentes/PostForm";
 import PostList from "./componentes/PostList";
-import React, { useState } from "react";
 import "./App.css";
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  //Cerrar seccion
-  const handleLogOut = () => {
-    event.preventDefault();
-    localStorage.removeItem("currentUser");
-    setUsername("");
-    setPassword("");
-    {
-      window.location.href = "/";
-    }
-  };
   //RENDERIZADO
   return (
     <div className="container">
@@ -29,7 +15,6 @@ function App() {
       <section>
         <PostList />
       </section>
-      <button onClick={handleLogOut}>Log Out</button>
     </div>
   );
 }
--- src/componentes/Nav.jsx
+++ src/componentes/Nav.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-/* import "../Styles/Navbar.css"; */
-
+import React, { useState } from "react";
+import "../Styles/Navbar.css";
 const Navbar = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  //Cerrar seccion
+  const handleLogOut = (event) => {
+    event.preventDefault();
+    localStorage.removeItem("currentUser");
+    setUsername("");
+    setPassword("");
+    {
+      window.location.href = "/";
+    }
+  };
   return (
     <nav className="navbar">
+      <button onClick={handleLogOut}>Log Out</button>
       <div className="navbar-logo">
         <img
           src="src\assets\Logo.png"
           alt="Logo"
-          style={{ width: "100px", height: "100px" }}
+          style={{ width: "50px", height: "50px" }}
         />
         <p>Written Time</p>
       </div>
